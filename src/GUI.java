f9d6e9554a40bb4a77965b7e3852c8f3a3390898
BT_1N3_18 <rohanp11@nycstudents.net>
2024-03-18 11:01:02 -0400

    Did gui

---
+++ src/GUI.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class GUI {
+    public GUI (){
+
+    }
+    public void greeting(){
+        System.out.println("Hello welcome to this chess game!!");
+    }
+
+}
